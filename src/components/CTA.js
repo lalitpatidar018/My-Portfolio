d6ec0cca4421a8911f2df0d489af2671a121ce7b
lalitpatidar018 <patidarlalit018@gmail.com>
2023-12-14 12:16:58 +0530

    my commit

---
+++ src/components/CTA.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function CTA() {
+  return (
+    <div className="cta">
+        <a href="https://drive.google.com/file/d/1XU7qhEGuNCPlkdGoeYLeCRsFWu1eOsJp/view?usp=sharing" target='_blank' className="btn">Download CV</a>
+        <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
---
+++ src/components/Social.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+
+
+function Social() {
+  return (
+    <div className='header_socials'>
+        <a href="https://www.linkedin.com/in/lalit-patidar-519617245" target="_blank"><BsLinkedin /></a>
+        <a href="https://github.com/lalitpatidar018" target="_blank"><FaGithub/></a>
+        
+    </div>
+  )
+}
+
+export default Social
